93f714358f14220f72e5fda42b5e40ff91cded2e
pradyun <pradyun.setti@gmail.com>
2023-01-11 09:29:33 +1300

    Add graphics setup code to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -9,14 +9,20 @@
 #include <nds.h>
 #include <nf_lib.h>
 
+#define TOP_SCREEN 0
+#define BOTTOM_SCREEN 1
 
 int main(int argc, char **argv) {
 
-	consoleDemoInit();		
-	consoleClear();			
-	setBrightness(3, 0);	
+	// init graphics and fs
+	NF_Set2D(TOP_SCREEN, 0);
+	NF_Set2D(BOTTOM_SCREEN, 0);
+	NF_SetRootFolder("NITROFS");
 
-	iprintf("\n Hello World!");
+	// init sprite system
+	NF_InitTiledBgBuffers();
+	NF_InitTiledBgSys(TOP_SCREEN);
+	NF_InitTiledBgSys(BOTTOM_SCREEN);
 
 	while(1) {
 
