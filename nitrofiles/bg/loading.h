19d963820592c42bccf6cccdfdf58767e2731bb0
pradyun <pradyun.setti@gmail.com>
2023-01-16 21:11:04 +1300

    centred loading background

--- nitrofiles/bg/loading.h
+++ nitrofiles/bg/loading.h
@@ -3,14 +3,14 @@
 
 //======================================================================
 //
-//	loading, 512x256@8, 
+//	loading, 256x256@8, 
 //	Transparent color : FF,00,FF
 //	+ palette 256 entries, not compressed
-//	+ 58 tiles (t|f reduced) not compressed
-//	+ regular map (in SBBs), not compressed, 64x32 
-//	Total size: 512 + 3712 + 4096 = 8320
+//	+ 51 tiles (t|f reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 3264 + 2048 = 5824
 //
-//	Time-stamp: 2023-01-16, 10:20:11
+//	Time-stamp: 2023-01-16, 21:09:24
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -19,11 +19,11 @@
 #ifndef GRIT_LOADING_H
 #define GRIT_LOADING_H
 
-#define loadingTilesLen 3712
-extern const unsigned int loadingTiles[928];
+#define loadingTilesLen 3264
+extern const unsigned int loadingTiles[816];
 
-#define loadingMapLen 4096
-extern const unsigned short loadingMap[2048];
+#define loadingMapLen 2048
+extern const unsigned short loadingMap[1024];
 
 #define loadingPalLen 512
 extern const unsigned short loadingPal[256];
